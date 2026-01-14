3f3a9bc398dfa2d8cdefaa2729b0827acfbd3da5
Brayan Sanchez <bsanchez100701@gmail.com>
2026-01-14 12:07:16 -0600

    Busqueda por id y Rango terminadas

--- src/components/Left-pannel.tsx
+++ src/components/Left-pannel.tsx
@@ -1,47 +1,121 @@
+import { useState } from "react";
 import RenderCard from "./RenderCard";
+//CHECKPOINT DE QUE FUNCIONA
 
 function LeftPannel() {
-  const init = 1;
-  const fin = 29;
-  const rango = fin - init + 1;
+  const [estructura, setEstructura] = useState({ init: 1, fin: 10 });
+
+  // const init = 280;
+  // const fin = 300;
+  // const rango = fin - init + 1;
+
+  function handleSubmitLeft() {
+    const textOne = document.getElementById(
+      "leftSearchByOne"
+    ) as HTMLInputElement | null;
+    const textTwo = document.getElementById(
+      "leftSearchByTwo"
+    ) as HTMLInputElement | null;
+    try {
+      const one = parseInt(textOne ? textOne.value : "1");
+      const two = parseInt(textTwo ? textTwo.value : "1");
+      setEstructura({
+        init: one,
+        fin: two,
+      });
+    } catch (error) {
+      alert("Piola");
+    }
+  }
+
   return (
     <>
-      {Array.from(new Array(rango).keys()).map((e) =>
-        (e + 2) % 3 === 0 ? (
-          <PlaceRow key={e} index={e} init={init} fin={fin} />
-        ) : (
-          <></>
+      <p>Búsqueda múltiple por rango de ID's</p>
+      <div className="row searchRow">
+        <div className="col-sm-4">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Mostrar desde"
+            aria-label="Search"
+            id="leftSearchByOne"
+          />
+        </div>
+        <div className="col-sm-4">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Hasta"
+            aria-label="Search"
+            id="leftSearchByTwo"
+          />
+        </div>
+        <div className="col-sm-4">
+          <button
+            className="btn btn-outline-success"
+            onClick={() => handleSubmitLeft()}
+          >
+            Buscar
+          </button>
+        </div>
+      </div>
+      {Array.from(new Array(estructura.fin - estructura.init + 1).keys()).map(
+        (e) => (
+          <Aux
+            key={e}
+            index={e + estructura.init}
+            fin={estructura.fin}
+            init={estructura.init}
+          />
         )
       )}
-      {/* <div className="row">
-        {Array.from(new Array(fin % 3).keys()).map((e) => (
-          <RenderCard key={e} index={(fin - e + 3).toString()} />
-        ))}
-      </div> */}
     </>
   );
 }
 
 interface PropsPlace {
   index: number;
+  fin: number;
   init: number;
+}
+
+function Aux({ index, fin, init }: PropsPlace) {
+  return (index + 2) % 3 === 0 || index === init ? (
+    <PlaceRow key={index} index={index} fin={fin} />
+  ) : (
+    <></>
+  );
+}
+
+interface PropsPlaceT {
+  index: number;
   fin: number;
 }
 
-function PlaceRow({ index, init, fin }: PropsPlace) {
-  console.log(index);
+function PlaceRow({ index, fin }: PropsPlaceT) {
   return (
     <>
       <div className="row">
-        {Array.from(new Array(3).keys()).map((e) =>
-          index + e <= fin ? (
-            <RenderCard key={index + e} index={(index + e).toString()} />
-          ) : (
-            <></>
-          )
-        )}
+        {Array.from(new Array(3).keys()).map((e) => (
+          <AuxTwo key={index + e} index={index} fin={fin} e={e} />
+        ))}
       </div>
     </>
+  );
+}
+
+interface PropsTwo {
+  index: number;
+  fin: number;
+  // init: number;
+  e: number;
+}
+
+function AuxTwo({ index, fin, e }: PropsTwo) {
+  return index + e <= fin ? (
+    <RenderCard index={(index + e).toString()} />
+  ) : (
+    <></>
   );
 }
 
--- src/components/Right-pannel.tsx
+++ src/components/Right-pannel.tsx
@@ -2,28 +2,38 @@ import { useState } from "react";
 import RenderCard from "./RenderCard";
 
 function RightPannel() {
-  return (
-    <>
-      <SearchBar />
-      <RenderCard index="417" />
-    </>
-  );
-}
+  const [renderValue, setRendervalue] = useState("1");
 
-function SearchBar() {
+  function handleSubmitRight() {
+    // e.preventDefault();
+    const input = document.getElementById(
+      "RightSearchBy"
+    ) as HTMLInputElement | null;
+    setRendervalue(input ? input.value : "");
+  }
+
   return (
     <>
-      <form className="d-flex" role="search" id="busqueda">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Busca por Id o Nombre"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success" type="submit">
-          Buscar
-        </button>
-      </form>
+      <div className="row searchRow">
+        <div className="col-sm-8">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Busca por Id o Nombre"
+            aria-label="Search"
+            id="RightSearchBy"
+          />
+        </div>
+        <div className="col-sm-4">
+          <button
+            className="btn btn-outline-success"
+            onClick={() => handleSubmitRight()}
+          >
+            Buscar
+          </button>
+        </div>
+      </div>
+      <RenderCard index={renderValue} />
     </>
   );
 }
